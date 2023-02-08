b9f693a0292a311e608ea15618b742091cf99bdd
danielthoren <danne_thoren456@hotmail.com>
2023-02-08 22:20:02 +0100

    Fixed conf.sh run_tests.sh and build.sh scripts
    Added condition that adds the folder ~/.local/bin if it does not exist
    since the cask installation is dependant on that directory.
    
    Added a export that adds the previously mentioned dir where the cask
    binary is located to the path for the runner scripts

--- conf.sh
+++ conf.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+binary_dir=~/.local/bin
+
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
 if ! command_exists cask ; then
     echo "Installing Cask..."
+
+    if [ ! -d $binary_dir ]; then
+        echo "Folder '${binary_dir}' does not exist, creating folder..."
+        mkdir -p $binary_dir
+    fi
 
     git clone https://github.com/cask/cask
     make -C cask install
